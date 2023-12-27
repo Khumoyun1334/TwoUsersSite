c7d7a3bc9dad71e6b2103f26efa5e0a285a7f298
Humoyunmirzo <you@exaple.com>
2023-12-27 22:38:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css"
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import Futter from "./components/Futter";
+const App = () => {
+  return (
+    <div>
+
+      <div>
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
