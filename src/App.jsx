1412a45153717aa5312ed9697f4d79c9acd6bf60
Humoyunmirzo <you@exaple.com>
2023-12-27 22:56:26 +0500

    ishlar bajarildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { Route, Routes } from "react-router-dom";
+
 import "./App.css"
 import React from "react";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Shop from "./pages/Shop";
-import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Futter from "./components/Futter";
 const App = () => {
   return (
     <div>
